dea31323768a273a7984834770f7d8c2b67a617f
Brandon Williams <mbw234@gmail.com>
2016-03-12 13:30:24 -0500

    Refined playground a bit.

--- optics.playground/Contents.swift
+++ optics.playground/Contents.swift
@@ -1,8 +1,14 @@
-import optics
+import XCPlayground
 import UIKit
+import optics
 
 let add: CGFloat -> CGFloat -> CGFloat = { a in { a + $0 } }
 let scale: CGFloat -> CGFloat -> CGFloat = { s in { s * $0 } }
+
+let holder = UIView()
+  |> UIView._backgroundColor *~ .brownColor()
+  <> UIView._frame • CGRect._size *~ CGSize(width: 200.0, height: 200.0)
+XCPlaygroundPage.currentPage.liveView = holder
 
 let baseStyle = UIButton._titleColor(.Normal) *~ .redColor()
   <> UIButton._titleText(.Normal) *~ "Howdy!"
@@ -10,21 +16,17 @@ let baseStyle = UIButton._titleColor(.Normal) *~ .redColor()
   <> UIView._frame *~ CGRect(x: 20.0, y: 20.0, width: 100.0, height: 50.0)
   <> UIView._frame • CGRect._size • CGSize._width %~ add(50.0)
   <> UIView._frame • CGRect._size • CGSize._height %~ scale(0.5)
-  <> UIView._layer • CALayer._cornerRadius *~ 10.0
-  <> UIView._layer • CALayer._masksToBounds *~ true
-
-let holder = UIView(frame: .unit)
-  |> UIView._backgroundColor *~ .brownColor()
-  <> UIView._frame • CGRect._size *~ CGSize(width: 200.0, height: 200.0)
 
 let circleStyle = UIView._frame • CGRect._size • CGSize._width *~ 100.0
   <> UIView._frame • CGRect._size • CGSize._height *~ 100.0
   <> UIView._layer • CALayer._cornerRadius *~ 50.0
   <> UIView._layer • CALayer._masksToBounds *~ true
 
-let button = UIButton(frame: .unit)
+let button = UIButton()
   |> baseStyle
-  |> circleStyle
 
 holder.addSubview(button)
 
+UIView.animateWithDuration(5.0) {
+  button |> circleStyle
+}
